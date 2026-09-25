7dc0972d5ee2eb5368bce023e201521ed9ad9561
jemyka <jemykapap@gmail.com>
2026-09-25 19:29:41 +0700

    Move and update cypress files to QA-Tugas18 API Automation using Cypress folder

--- QA-Tugas18 API Automation using Cypress/cypress/e2e/Api Automation/APICategories.cy.js
+++ QA-Tugas18 API Automation using Cypress/cypress/e2e/Api Automation/APICategories.cy.js
@@ -5,69 +5,63 @@ describe("API Testing", () => {
     cy.request("GET", "https://api.escuelajs.co/api/v1/categories").then(
       (response) => {
         expect(response.status).to.eq(200);
-        expect(response.responseTime).to.be.bellow(2000);
-        expect(response.responseSize).to.be.abbove(500);
       },
     );
   });
   it("TC002-Get Categories by Limit with 2 Id", () => {
-    cy.request(
-      "GET",
-      "https://api.escuelajs.co/api/v1/categories?limit=2",
-    ).then((response) => {
+    cy.request({
+      method: "GET",
+      url: "https://api.escuelajs.co/api/v1/categories?limit=2",
+    }).then((response) => {
       expect(response.status).to.eq(200);
-      expect(response.text).to.include("id");
       expect(response.body).to.have.length(2);
     });
   });
   it("TC003-Get Categories with Valid Id", () => {
-    cy.request("GET", "https://api.escuelajs.co/api/v1/categories/5").then(
-      (response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.not.be.null;
-        expect(response.body.name).to.eq("Miscellaneous");
-      },
-    );
+    cy.request({
+      method: "GET",
+      url: "https://api.escuelajs.co/api/v1/categories/5",
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.not.be.null;
+      expect(response.body.name).to.eq("Miscellaneous");
+    });
   });
   it("TC004-Get Categories with Invalid Id", () => {
-    cy.request("GET", "https://api.escuelajs.co/api/v1/categories/999").then(
-      (response) => {
-        const bodyText = (
-          typeof response.body === "object"
-            ? JSON.stringify(response.body)
-            : String(response.body)
-        ).toLowerCase();
-        expect(response.status).to.be.oneOf([400, 404]);
-        // Assertion menggunakan .satisfy()
-        expect(response.body.text).to.satisfy(text);
-        text.includes("error");
-        text.includes("not found");
-        text.includes("category not found");
-      },
-    );
+    cy.request({
+      method: "GET",
+      url: "https://api.escuelajs.co/api/v1/categories/999",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.not.be.null;
+      expect(response.body.name).to.eq("EntityNotFoundError");
+    });
   });
   it("TC005-Get Kategori dengan ID Non-Numerik", () => {
-    cy.request("GET", "https://api.escuelajs.co/api/v1/categories/jemyka").then(
-      (response) => {
-        expect(response.status).to.be.oneOf([400, 404]);
-        expect(response.body.error).to.eq("Bad Request");
-        expect(response.body.message).to.eq(
-          "Validation failed (numeric string is expected)",
-        );
-      },
-    );
+    cy.request({
+      method: "GET",
+      url: "https://api.escuelajs.co/api/v1/categories/jemyka",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.error).to.eq("Bad Request");
+      expect(response.body.message).to.eq(
+        "Validation failed (numeric string is expected)",
+      );
+    });
   });
   it("TC006-Membuat Kategori Baru", () => {
     cy.request({
       method: "POST",
       url: "https://api.escuelajs.co/api/v1/categories/",
       body: {
-        name: "BeautyStore",
+        name: "T-Shirt Burgundy",
         image: "https://placeimg.com/640/480/any",
       },
     }).then((response) => {
       expect(response.status).to.eq(201);
-      expect(response.body.name).to.eq(BeautyStore);
+      expect(response.body).to.have.property("name");
     });
   });
   it("TC007-Membuat Kategori Tanpa Field Name", () => {
@@ -77,6 +71,7 @@ describe("API Testing", () => {
       body: {
         image: "https://placeimg.com/640/480/any",
       },
+      failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.eq(500);
     });
@@ -86,14 +81,13 @@ describe("API Testing", () => {
       method: "POST",
       url: "https://api.escuelajs.co/api/v1/categories/",
       body: {
-        name: "Makeup",
-        image: "https://placeimg.com/640/480/anu",
+        name: "Perkakas",
+        image: "bukan-url-gambar",
       },
+      failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.eq(400);
-      expect(response.body.text).to.satisfy(text);
-      text.includes("image");
-      text.includes("url");
+      expect(response.body).to.have.property("message");
     });
   });
   it("TC009-Update Kategori dengan ID Valid", () => {
@@ -115,32 +109,29 @@ describe("API Testing", () => {
       body: {
         name: "Avocado Juice Update",
       },
+      failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.be.oneOf([400, 404]);
       expect(response.body).to.have.property("message");
     });
   });
   it("TC011-Get Produk dengan ID Kategori 55", () => {
-    cy.request(
-      "GET",
-      "https://api.escuelajs.co/api/v1/categories/55/products",
-    ).then((response) => {
+    cy.request({
+      method: "GET",
+      url: "https://api.escuelajs.co/api/v1/categories/55/products",
+    }).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body).to.be.an("array");
     });
   });
   it("TC012-Hapus Kategori by ID", () => {
-    cy.request("DELETE", "https://api.escuelajs.co/api/v1/categories/1").then(
-      (response) => {
-        const bodyText = (
-          typeof response.body === "object"
-            ? JSON.stringify(response.body)
-            : String(response.body)
-        ).toLowerCase();
-        expect(response.status).to.eq(400);
-        expect(response.body.text).to.satisfy(text);
-        text.includes("FOREIGN KEY constraint failed");
-      },
-    );
+    cy.request({
+      method: "DELETE",
+      url: "https://api.escuelajs.co/api/v1/categories/1",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.code).to.eq("SQLITE_CONSTRAINT_FOREIGNKEY");
+    });
   });
 });
